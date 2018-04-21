70335e37e4e484824793437ce8d24aced84e9610
camous <camous.fabien@gmail.com>
2018-04-21 20:49:18 +0200

    route/from/to return resource usage

--- routes/route.js
+++ routes/route.js
@@ -1,13 +1,21 @@
-
-
 const express = require('express');
+var _ = require('lodash');
 const app = express.Router();
 
 app.get('/:from/:to', function (req,res){
+    var carnival = req.app.get('carnival');
+
     var from = req.params.from;
     var to = req.params.to;
     var route = req.app.get('routes').path(from,to);
-    res.status(200).send(route);
+
+    var crossedTerritories = {};
+    for(var id in route){
+        var territory = route[id];
+        crossedTerritories[territory] = carnival.territories[territory].meta;
+    }
+
+    res.status(200).send({ route : route, territories : crossedTerritories});
 });
 
 module.exports = app;
