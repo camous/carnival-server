24c7f34b64d58ed9d3bfd069e5774a50e8eccde6
camous <camous.fabien@gmail.com>
2018-04-21 20:50:09 +0200

    adding objective controller

---
+++ modules/objective.js
@@ -0,0 +1,36 @@
+var _ = require('lodash');
+
+/**
+ * Calculate objective cost
+ * @param {*} carnival 
+ * @param {*} objective 
+ */
+function getObjectiveCost(carnival, objective) {
+    return carnival.game.rules.cost_per_objective + objective.resource;
+}
+
+/**
+ * Calculate cost of all objectives
+ * @param {*} carnival 
+ * @param {*} objectives 
+ */
+function getAllObjectivesCost(carnival, objectives){
+    var cost = 0;
+    _.forEach(objectives, function(value, key){
+        cost += getObjectiveCost(carnival, value);
+    });
+
+    return cost;
+}
+
+function deleteObjective(carnival, team, roundid, objectiveid){
+    var teamobjectives = carnival.game.rounds[roundid-1].objectives[team];
+    var deletedobject = teamobjectives[objectiveid];
+    carnival.game.rounds[roundid-1].objectives[team] = teamobjectives.splice(objectiveid,1);
+
+    return deletedobject;
+}
+
+module.exports.getObjectiveCost = getObjectiveCost;
+module.exports.getAllObjectivesCost = getAllObjectivesCost;
+module.exports.deleteObjective = deleteObjective;
--- routes/objective.js
+++ routes/objective.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express.Router();
 var _ = require('lodash');
+const objectives = require('../modules/objective');
 
 app.get('/:roundid/:team', function(req,res){
     var carnival = req.app.get('carnival');
@@ -8,6 +9,47 @@ app.get('/:roundid/:team', function(req,res){
     var team = req.params.team;
 
     res.status(200).send(carnival.game.rounds[roundid-1].objectives[team]);
+});
+
+app.put('/:roundid/:team/:objectiveid?', function (req, res){
+    var carnival = req.app.get('carnival');
+    var objectiveid = req.params.objectiveid;
+    var roundid = req.params.roundid;
+    var team = req.params.team;
+
+    var teamobjectives = carnival.game.rounds[roundid-1].objectives[team];
+
+    // it's a new objective
+    if(objectiveid === undefined){
+        teamobjectives.push(req.body);
+        objectiveid = teamobjectives.length-1;
+    }else{
+        teamobjectives[objectiveid] = req.body;
+    }
+
+    res.status(200).send(teamobjectives[objectiveid]);
+});
+
+app.get('/:roundid/:team/cost', function (req,res){
+    var carnival = req.app.get('carnival');
+    var roundid = req.params.roundid;
+    var team = req.params.team;
+    var teamobjectives = carnival.game.rounds[roundid-1].objectives[team];
+
+    var cost = objectives.getAllObjectivesCost(carnival, teamobjectives);
+
+    res.status(200).send({ objectivescount : teamobjectives.length, cost : cost});
+});
+
+app.delete('/:roundid/:team/:objectiveid', function (req, res){
+    var carnival = req.app.get('carnival');
+    var roundid = parseInt(req.params.roundid);
+    var objectiveid = parseInt(req.params.objectiveid);
+    var team = req.params.team;
+
+    objectives.deleteObjective(carnival, team, roundid, objectiveid)
+
+    res.status(200).send(carnival.game.rounds[roundid-1].objectives[team]) ;
 });
 
 module.exports = app;
